c44f8fbc93468c88a50bad623ab791c744ae0acc
o-louis <oriane.louis.pro@gmail.com>
2021-02-24 21:44:47 -0500

    feat(Header): Add some classes style

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,5 +1,5 @@
-const Header = ({ title }) => (
-  <header>
+const Header = ({ title, ...props }) => (
+  <header {...props}>
     <h1>{title}</h1>
   </header>
 );
--- components/Layouts/Layouts.jsx
+++ components/Layouts/Layouts.jsx
@@ -4,7 +4,10 @@ import Header from "../Header/Header";
 export default function Layout({ children }) {
   return (
     <>
-      <Header title="Pokedex" />
+      <Header
+        title="Pokedex"
+        className="w-full h-14 bg-red-700 flex justify-center items-center text-white uppercase text-xl font-bold tracking-wide"
+      />
       <main>{children}</main>
       <Footer description="2021. Oriane" />
     </>
