c2e3411a3c87f4f734a2f7f6eab70c5c77c5a319
o-louis <oriane.louis.pro@gmail.com>
2021-02-24 21:57:09 -0500

    feat(Layouts): Set a main background

--- components/Layouts/Layouts.jsx
+++ components/Layouts/Layouts.jsx
@@ -3,12 +3,12 @@ import Header from "../Header/Header";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-yellow-50">
       <Header
         title="Pokedex"
         className="w-full h-14 bg-red-700 flex justify-center items-center text-white uppercase text-xl font-bold tracking-wide"
       />
-      <main class="mb-auto">{children}</main>
+      <main className="mb-auto">{children}</main>
       <Footer
         description="2021. Oriane"
         className="w-full h-14 bg-red-700 flex justify-center items-center text-white"
