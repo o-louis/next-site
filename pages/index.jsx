df3fc7dc31bee8c0a5fcc964c1506fb56883171a
o-louis <oriane.louis.pro@gmail.com>
2021-02-24 23:47:22 -0500

    feat(pokemon-url): Retrieve the id from the url w/ regex

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,8 +11,19 @@ const Pokedex = ({ pokemons }) => {
 };
 
 export async function getStaticProps() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
-  const pokemons = await response.json();
+  const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=150";
+  const IMAGE_URL = "https://pokeres.bastionbot.org/images/pokemon/:id.png";
+
+  const response = await fetch(API_URL);
+  const { results } = await response.json();
+
+  const regex = /\/pokemon\/(\d+)\//;
+  const pokemons = results.map(({ name, url }) => {
+    let img = null;
+    if (url.match(regex).length)
+      img = IMAGE_URL.replace(":id", url.match(regex)[1]);
+    return { name, img };
+  });
   return { props: { pokemons } };
 }
 
