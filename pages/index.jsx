6de6ea84cd02b06912e77ac97ab5d4f400f875e0
o-louis <oriane.louis.pro@gmail.com>
2021-02-24 21:24:02 -0500

    refactor(index): Change js for jsx extension

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Pokedex</title>
       </Head>
     </div>
   )
