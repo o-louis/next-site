193d7189f5aabe95aca792a2a18f0bdc77e2b446
o-louis <oriane.louis.pro@gmail.com>
2021-02-24 22:06:25 -0500

    feat(pokemon-api): Fetch data from the api with getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,19 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-export default function Home() {
+const Pokedex = ({ pokemons }) => {
   return (
     <div className="container">
       <Head>
         <title>Pokedex</title>
       </Head>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=150");
+  const pokemons = await response.json();
+  return { props: { pokemons } };
 }
+
+export default Pokedex;
